66f2bbd4e63d9a1e2d3f22a2f71d9d3e54842285
asmarcheva <78663281+asmarcheva@users.noreply.github.com>
2022-01-26 10:39:33 -0500

    Update and rename 06-data-explore.R to 04-data-explore.R

--- code/04-data-explore.R
+++ code/04-data-explore.R
@@ -8,22 +8,29 @@ library(ggplot2)
 library(fixest)
 
 
-load("./data/intermediate/drought_df.RData")
+load("./data/intermediate/main_df.RData")
 load("./data/intermediate/centroid.RData")
+load("./data/intermediate/drought_df.RData")
 
 #Redefine drought-days to be positive
-main_df$drought_day<- (-1)*main_df$drought_day
+drought_df$drought_day<- (-1)*drought_df$drought_day
+
+#Merge the main dataframe and drought dataframe
+main_df<-merge(main_df, drought_df, by=c("year","GeoFIPS"))
 
 #Question 1: How persistent are drought-days?
 
+main_df<-main_df[with(main_df, order(GeoFIPS,year)),]
+
 main_df<-main_df %>%
   group_by(GeoFIPS) %>%
-  dplyr::mutate(lag1 = dplyr::lag(drought_day,n=1,default=NA)) %>%
-  dplyr::mutate(lag2 = dplyr::lag(drought_day,n=2,default=NA)) %>%
-  dplyr::mutate(lag3 = dplyr::lag(drought_day,n=3,default=NA)) %>%
-  dplyr::mutate(lag4 = dplyr::lag(drought_day,n=4,default=NA)) %>%
-  dplyr::mutate(lag5 = dplyr::lag(drought_day,n=5,default=NA)) %>%
+  dplyr::mutate(lag1 = dplyr::lag(drought_day,n=1)) %>%
+  dplyr::mutate(lag2 = dplyr::lag(drought_day,n=2)) %>%
+  dplyr::mutate(lag3 = dplyr::lag(drought_day,n=3)) %>%
+  dplyr::mutate(lag4 = dplyr::lag(drought_day,n=4)) %>%
+  dplyr::mutate(lag5 = dplyr::lag(drought_day,n=5)) %>%
   as.data.frame()
+
 
 #NOTE: mutate adds new variables and preseves existing ones; transmute adds new 
 #variables and drops existing ones.
@@ -107,7 +114,6 @@ main_df %>%
 #West has greater variance: east:  2.22 west: 2.40
 
 #Check average drought-days over time for east and west
-year_v<-1969:2018
 
 dd_by_year<-main_df %>%
   group_by(year) %>%
@@ -117,11 +123,14 @@ dd_by_year_west<-main_df %>%
   filter(west == 1, na.rm = TRUE) %>%
   group_by(year) %>%
   summarise_at(vars(drought_day), list(name = mean))
+dd_by_year_west<-dd_by_year_west[1:50,]
 
 dd_by_year_east<-main_df %>%
   filter(west == 0, na.rm = TRUE) %>%
   group_by(year) %>%
   summarise_at(vars(drought_day), list(name = mean))
+dd_by_year_east<-dd_by_year_east[1:50,]
+
 
 plot(dd_by_year_west, main="SPEI-months by year, Eastern and Western Counties",
      xlab="Year ", ylab="SPEI-months ", pch=19)
@@ -133,11 +142,18 @@ res<-lm(name~year, data=dd_by_year_east)
 summary(res)
 
 #No apparent trends: average SPEI over year is just about zero.
-
+plot(dd_by_year_west, main="SPEI-months by year, Eastern and Western Counties",
+     xlab="Year ", ylab="SPEI-months ", pch=19)
+points(dd_by_year_east, col = "green", pch = 8)
+abline(lm(dd_by_year_west$name~dd_by_year_west$year), col="black")
+abline(lm(dd_by_year_east$name~dd_by_year_east$year), col="green")
+legend("topright",legend=c("West", "East"), 
+       fill = c("black","green")
+)
 
 #Question: How does drought affect average farm and non-farm income?
 
-#Scatter, partialling out fixed effects
+#Partial out fixed effects
 main_df$log_farmincome<-log(main_df$farm_income)
 main_df$log_nonfarmincome<-log(main_df$nonfarm_income)
 
@@ -146,15 +162,78 @@ main_df[,c("drought_day_demean","farm_income_demean", "nonfarm_income_demean")] 
   demean(X = main_df[,c("drought_day","log_farmincome","log_nonfarmincome")],
          f = main_df[, c("GeoFIPS", "year")],na.rm = FALSE)
 
-(ggplot(main_df, aes(x=drought_day_demean,y=farm_income_demean)) +
-    geom_point(alpha = 0.4) +
-    stat_summary_bin(fun='mean', bins=20,
-                     color='orange', size=2, geom='point'))
+#Make binscatters by grouping data by deciles.
+total_ddd_decile<-main_df %>%
+  mutate(decile = ntile(drought_day_demean, 10)) %>%
+  group_by(decile) %>% 
+  summarise(across(drought_day_demean:nonfarm_income_demean, mean))
 
-(ggplot(main_df, aes(x=drought_day_demean,y=nonfarm_income_demean)) +
-    geom_point(alpha = 0.4) +
-    stat_summary_bin(fun='mean', bins=20,
-                     color='orange', size=2, geom='point'))
+plot(total_ddd_decile$drought_day_demean, total_ddd_decile$farm_income_demean,
+  main="Farm Income by drought days: deciles, all counties",
+  ylab="Demeaned farm income, log millions",
+  xlab="Demeaned drought months",
+  pch=19)
+reg_model<-lm(farm_income_demean~drought_day_demean, data=total_ddd_decile)
+reg_model
+abline(reg_model, col="black")
+
+plot(total_ddd_decile$drought_day_demean, total_ddd_decile$nonfarm_income_demean,
+     main="Non-farm Income by drought days: deciles, all counties",
+     ylab="Demeaned nonfarm income, log millions",
+     xlab="Demeaned drought months",
+     pch=19)
+reg_model<-lm(nonfarm_income_demean~drought_day_demean, data=total_ddd_decile)
+reg_model
+abline(reg_model, col="black")
+
+west_ddd_decile<-main_df %>%
+  filter(west == 1) %>%
+  mutate(decile = ntile(drought_day_demean, 10)) %>%
+  group_by(decile) %>% 
+  summarise(across(drought_day_demean:nonfarm_income_demean, mean))
+
+east_ddd_decile<-main_df %>%
+  filter(west == 0) %>%
+  mutate(decile = ntile(drought_day_demean, 10)) %>%
+  group_by(decile) %>% 
+  summarise(across(drought_day_demean:nonfarm_income_demean, mean))
+
+plot(west_ddd_decile$drought_day_demean, west_ddd_decile$farm_income_demean,
+     main="Farm Income by drought days: deciles, Western counties",
+     ylab="Demeaned farm income, log millions",
+     xlab="Demeaned drought months",
+     pch=19)
+reg_model<-lm(farm_income_demean~drought_day_demean, data=west_ddd_decile)
+reg_model
+abline(reg_model, col="black")
+
+plot(west_ddd_decile$drought_day_demean, west_ddd_decile$nonfarm_income_demean,
+     main="Non-farm Income by drought days: deciles, Western counties",
+     ylab="Demeaned nonfarm income, log millions",
+     xlab="Demeaned drought months",
+     pch=19)
+reg_model<-lm(nonfarm_income_demean~drought_day_demean, data=west_ddd_decile)
+reg_model
+abline(reg_model, col="black")
+
+plot(east_ddd_decile$drought_day_demean, east_ddd_decile$farm_income_demean,
+     main="Farm Income by drought days: deciles, Eastern counties",
+     ylab="Demeaned farm income, log millions",
+     xlab="Demeaned drought months",
+     pch=19)
+reg_model<-lm(farm_income_demean~drought_day_demean, data=east_ddd_decile)
+reg_model
+abline(reg_model, col="black")
+
+plot(east_ddd_decile$drought_day_demean, east_ddd_decile$nonfarm_income_demean,
+     main="Non-farm Income by drought days: deciles, Eastern counties",
+     ylab="Demeaned nonfarm income, log millions",
+     xlab="Demeaned drought months",
+     pch=19)
+reg_model<-lm(nonfarm_income_demean~drought_day_demean, data=east_ddd_decile)
+reg_model
+abline(reg_model, col="black")
+
 
 #Run the first regression: how does farm income change with SPEI-months?
 
@@ -168,6 +247,14 @@ results<-fixest::feols(log_nonfarmincome~drought_day|year+GeoFIPS, main_df,
 
 summary(results)
 
+#How does total income change with SPEI-months?
+main_df$log_income<-log(main_df$income)
+
+results<-fixest::feols(log_income~drought_day|year+GeoFIPS, main_df, 
+                       cluster=~GeoFIPS)
+
+summary(results)
+
 #Run the second regression: how does farm income change with lagged SPEI-months?
 
 results<-fixest::feols(log_farmincome~lag1|year+GeoFIPS, main_df, 
@@ -175,8 +262,29 @@ results<-fixest::feols(log_farmincome~lag1|year+GeoFIPS, main_df,
 
 summary(results)
 
+results<-fixest::feols(log_farmincome~drought_day|year+GeoFIPS, data=subset(main_df, west==1), 
+                       cluster=~GeoFIPS)
+summary(results)
+
+results<-fixest::feols(log_farmincome~drought_day|year+GeoFIPS, data=subset(main_df, west==0), 
+                       cluster=~GeoFIPS)
+summary(results)
+
+
 results<-fixest::feols(log_nonfarmincome~lag1|year+GeoFIPS, main_df, 
                        cluster=~GeoFIPS)
 
 summary(results)
+
+
+results<-fixest::feols(log_nonfarmincome~drought_day|year+GeoFIPS, data=subset(main_df, west==1), 
+                       cluster=~GeoFIPS)
+
+summary(results)
+
+results<-fixest::feols(log_nonfarmincome~drought_day|year+GeoFIPS, data=subset(main_df, west==0), 
+                       cluster=~GeoFIPS)
+
+summary(results)
+
 
