97cfa0d288b47fb619dca9fbd8175c2a8bdd9373
asmarcheva <78663281+asmarcheva@users.noreply.github.com>
2022-03-14 15:16:31 -0400

    Update 03-drought_days.R

--- code/03-drought_days.R
+++ code/03-drought_days.R
@@ -1,11 +1,23 @@
 ##This code calculates a by-county measure of SPEI and also determines the 
 ##measure of drought used.
 
+##The calculations will be done on as many of the time intervals of the SPEI
+##as exist in the data
+
+#################### User Defined Inputs ########################################
+
 setwd("C:\\Users\\anton\\Dropbox\\drought")
 
 #GitHub file to save dataframes to:
 int_folder = "C:\\Users\\anton\\Documents\\GitHub\\drought\\data\\intermediate\\"
 
+#Which time-scales of SPEI are we calculating?
+time_scale = c("01","12")
+
+#What is the cutoff level for a drought?
+drought_cutoff = -1
+
+################################################################################
 
 library(tidyverse)
 library(lubridate)
@@ -14,19 +26,37 @@ library(terra)
 library(dplyr)
 
 #Load Data
-load("./data/intermediate/raw_df.RData")
-load("./data/intermediate/time_mat.RData")
+load("./data/intermediate/main_df.RData")
+###load("./data/intermediate/time_mat.RData")
 county_v<-vect("./data/intermediate/ county_v.GTiff")
 load("./data/intermediate/spei_by_month.RData")
 
-#Initialize the drought dataframe:
+#Initialize the full drought dataframe
 drought_df<-main_df[,c("GeoFIPS", "year")]
+
+for (ts in time_scale){
+  filename = paste(int_folder, "spei", ts, "_by_month.RData", sep = "")
+  load(filename)
+  #Initialize the drought dataframe for the timescale:
+  mini_drought_df<-as.data.frame(matrix(ncol = 3, nrow = 0))
+  spei_varname = paste("drought_month", ts, sep = "")
+  colnames(mini_drought_df)<-c("GeoFIPS", "year", spei_varname)
+  
+  #The following loop adds together the spei when the spei is less than -1
+  for (y in 1969:2018){
+    for (f in county_v$GEOID){
+      county_year<-filter(spei_by_month, fips == f & year == y)
+      drought_month<-sum(county_year[county_year$spei<drought_cutoff,]$spei, na.rm=TRUE)
+      mini_drought_df[nrow(drought_df) + 1,] <- c(f,y,drought_month)
+      }
+    print(y)
+  }
+  main_df<-merge(drought_df, mini_drought_df, by=c("GeoFIPS", "year"), all = TRUE)
+}
 
 #Different ways to calculate drought:
 
-#1. Binary variable for whether there was drought in a year.
-#What is the cutoff level for a drought?
-drought_cutoff = -1
+#Indicator for whether there was a drought in the year
 
 # drought_df$drought<-0
 # for (y in 1969:2018){
@@ -38,21 +68,6 @@ drought_cutoff = -1
 #   print(y)
 # }
 
-
-#2. "Drought-days" measure: add the amount of drought in a month less than -1.
-
-drought_df$drought_day<-0
-for (y in 1969:2018){
-  for (f in county_v$GEOID){
-    county_year<-filter(spei_by_month, fips == f & year == y)
-    drought_day<-sum(county_year[county_year$spei<drought_cutoff,]$spei, na.rm=TRUE)
-    drought_df[drought_df$year == y & drought_df$GeoFIPS == f, "drought_day"]<-drought_day
-  }
-  print(y)
-}
-
-##Filter drought_df only for those values in county_v$GEOID
-drought_df<- filter(drought_df, GeoFIPS %in% county_v$GEOID)
 
 #Save files
 save(drought_df, file = paste(int_folder, "drought_df.RData", sep = "", collapse = NULL))
