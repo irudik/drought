dd0c5dc3669226c0baad827eb5f727ad92a22389
asmarcheva <78663281+asmarcheva@users.noreply.github.com>
2022-03-14 15:15:03 -0400

    Update 02-process-spei.R

--- code/02-process-spei.R
+++ code/02-process-spei.R
@@ -1,7 +1,16 @@
-setwd("C:\\Users\\anton\\Dropbox\\drought")
+#This code creates drought variables with my SPEI data. SPEI data exists for
+#timescales from 1 month to 48 months.
 
-#GitHub file to save dataframes to:
-int_folder = "C:\\Users\\anton\\Documents\\GitHub\\drought\\data\\intermediate\\"
+#################### User Defined Inputs #######################################
+#Input the timescales of SPEI to process, as strings below:
+spei_timescales<-c("01","12")
+#Input directory where drought folder exists
+setwd("C:\\Users\\anton\\Dropbox\\drought")
+#Input directory to save files to.
+int_folder = ".\\data\\intermediate\\"
+################################################################################
+
+#Libraries
 
 library(tidyverse)
 library(tidyr)
@@ -9,41 +18,46 @@ library(terra)
 library(dplyr)
 library(lubridate)
 
-#Get SPEI data
-spei_r <- rast('./data/raw/spei01.nc')
-
-#Project the county data to be the same as the drought data
-project(county_v, spei_r)
-
-#Match the spatial extents
-spei_r <- crop(spei_r,e)
-
-#Calculate the SPEI by county-month
-#Calculating County aggregate SPEI by SIMPLE AVERAGE
-
-spei_by_month<-data.frame()
-spei_by_onemonth<-data.frame(county_v$GEOID)
-colnames(spei_by_onemonth)<-"fips"
-
-#This block takes spei and averages it over county boundaries for every month,
-
-#t = 817: Jan 1969 t = 1416: final observation
-for (t in 817:1416){
-  layer = spei_r[[t]]
-  spei_by_county = extract(layer,county_v, mean, method = "simple", weights = TRUE)
-  spei_by_county<-data.frame(spei_by_county)
-  colnames(spei_by_county)<-c("ID","spei")
-  spei_by_onemonth$spei<-spei_by_county$spei
-  spei_by_onemonth$year<-year(time(spei_r)[t])
-  spei_by_onemonth$month<-month(time(spei_r)[t])
-  print(t)
-  spei_by_month<-rbind(spei_by_month, spei_by_onemonth)
-}
-
-##CHECK TO MAKE SURE THAT THE IDS CORRESPOND TO THE CORRECT FIPS CODES!
-
-save(spei_by_month, file = paste(int_folder, "spei_by_month.RData", sep = "", collapse = NULL))
-writeRaster(spei_r,  filename = paste(int_folder, "spei_r.tif", sep = "", collapse = NULL))
-time_mat<-time(spei_r)
+for (ts in spei_timescales){
+  filename<-paste("./data/raw/spei/spei",ts,".nc",sep="")
+  #Get SPEI data
+  spei_r <- rast(filename)
+  
+  #Project the county data to have the same coordinates as the drought data
+  county_v<-vect("./data/intermediate/ county_v.GTiff")
+  project(county_v, spei_r)
+  #Match the spatial extents
+  e <- ext(-150,-40,20,60) #This spatial extent shrinks the filesize to just US
+  spei_r <- crop(spei_r,e)
+  
+  #Calculate the SPEI by county-month
+  #Calculating County aggregate SPEI by SIMPLE AVERAGE
+  
+  spei_by_month<-data.frame()
+  spei_by_onemonth<-data.frame(county_v$GEOID)
+  colnames(spei_by_onemonth)<-"fips"
+  
+  #This block takes spei and averages it over county boundaries for every month,
+  
+  #t = 817: Jan 1969 t = 1416: final observation
+  for (t in 817:1416){
+    layer = spei_r[[t]]
+    spei_by_county = extract(layer,county_v, mean, method = "simple", weights = TRUE) #This line does the calculation
+    spei_by_county<-data.frame(spei_by_county)
+    colnames(spei_by_county)<-c("ID","spei")
+    spei_by_onemonth$spei<-spei_by_county$spei
+    spei_by_onemonth$year<-year(time(spei_r)[t])
+    spei_by_onemonth$month<-month(time(spei_r)[t])
+    print(t)
+    spei_by_month<-rbind(spei_by_month, spei_by_onemonth)
+  }
+  
+  save(spei_by_month, file = paste(int_folder, "spei",ts,"_by_month.RData",sep="", collapse = NULL))
+  writeRaster(spei_r,  filename = paste(int_folder, "spei",ts,"_r.tif", sep = "", collapse = NULL))
+}  
+time_mat<-time(spei_r) #The spei file saves without the time frame. It is the same for each iteration of the loop.
 save(time_mat, file = paste(int_folder, "time_mat.RData", sep = "", collapse = NULL))
+
+
+
 
